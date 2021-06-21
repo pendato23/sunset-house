d4650ac5a3166fe7fdeb32a7b9b5e2e730dc2f25
simigeum <ldu50@naver.com>
2021-06-21 14:30:48 +0900

    ✨ Implement sidebar popup and slideToggle by using jquery

--- js/main.js
+++ js/main.js
@@ -4,4 +4,28 @@ $(document).ready(function () {
     $('.my-menu-content').fadeToggle(200)
     // $('.my-menu-content').addClass('on') 트랜지션 효과는 불가.
   })
+
+  //sidebar
+  $('.gnb .ic-menu').click(function () {
+    $('.sidebar').addClass('is-active')
+    $('.bg').addClass('is-active')
+  })
+  $('.bg').click(function () {
+    $('.sidebar').removeClass('is-active')
+    $('.bg').removeClass('is-active')
+  })
+
+  //sidebar 버튼
+  $('.drawer-menu-button').click(function () {
+    $(this).next('.drawer-menu-content').slideToggle(200)
+    $(this).find('.ic-chevron').toggleClass('is-active')
+    $(this)
+      .next('.drawer-menu-content')
+      .siblings('.drawer-menu-content')
+      .slideUp()
+    $(this)
+      .siblings('.drawer-menu-button')
+      .find('.ic-chevron')
+      .removeClass('is-active')
+  })
 })
