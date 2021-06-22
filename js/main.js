c2be9d53a4fc371af5400395f05dea42b61d0e55
simigeum <ldu50@naver.com>
2021-06-22 21:25:31 +0900

    🐛 Delete scroll chain and annotation

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   //my-menu 버튼
   $('.my-menu-button').click(function () {
     $('.my-menu-content').fadeToggle(200)
-    // $('.my-menu-content').addClass('on') 트랜지션 효과는 불가.
   })
 
   //sidebar
