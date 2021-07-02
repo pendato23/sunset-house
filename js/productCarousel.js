90ff3129a7ebc04e661331235c81e236ebfd61bc
simigeum <ldu50@naver.com>
2021-07-02 13:28:03 +0900

    🚧 Import tiny-slider.js to the project and initialize productCarousel

---
+++ js/productCarousel.js
@@ -0,0 +1,12 @@
+const productCarousel = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  asNavThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPauser: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
