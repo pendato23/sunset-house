33518fe614da2d158522509f88da9596e3189f10
simigeum <ldu50@naver.com>
2022-08-28 18:50:05 +0900

    ♻️ Convert jquery into javascript in sidebarToggling

--- js/main.js
+++ js/main.js
@@ -4,16 +4,6 @@ $(document).ready(function () {
     $('.my-menu').toggleClass('is-active')
   })
 
-  //sidebar
-  $('.gnb .ic-menu').click(function () {
-    $('.sidebar').addClass('is-active')
-    $('.overlay').addClass('is-active')
-  })
-  $('.overlay').click(function () {
-    $('.sidebar').removeClass('is-active')
-    $('.overlay').removeClass('is-active')
-  })
-
   //sidebar 버튼
   $('.drawer-menu').click(function () {
     $(this).toggleClass('is-open')
